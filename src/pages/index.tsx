c11b9a9a47eebd5b4103c74e34b49cfe90198987
binithekid <binitek1@gmail.com>
2024-07-20 19:05:54 +0100

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,117 +1,155 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { FaInstagram, FaTiktok, FaTwitter } from "react-icons/fa";
+import phone from "../../public/phoneshot.png";
+import { IoMailOutline } from "react-icons/io5";
+import { motion } from "framer-motion";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="w-full flex flex-col gap-4 overflow-hidden h-screen px-40">
+      <div className="w-full flex flex-row justify-between py-8 items-center">
+        <h1 className="text-3xl font-bold tracking-tighter">SYNC</h1>
+        <motion.button
+          whileHover={{
+            y: -2,
+            transition: { duration: 0.1, ease: "easeOut", bounce: 0.4 },
+          }}
+          className="flex flex-row gap-2 bg-white shadow text-sm items-center px-4 py-2 border rounded-full border-gray-400"
+        >
+          <IoMailOutline className="text-2xl" />
+          <p className="tracking-tight text-gray-800">Get in touch</p>
+        </motion.button>
+      </div>
+      <div className="flex flex-row w-full flex-grow">
+        <motion.div
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.2, duration: 0.4, ease: "easeInOut" }}
+          className="flex-grow flex justify-center flex-col overflow-hidden gap-2"
+        >
+          <h1 className="font-bold pb-2 tracking-tight md:text-6xl bg-gradient-to-b from-black to-gray-400 bg-clip-text text-transparent">
+            Create <span className="text-blue-500">Events</span> in Real Time
+          </h1>
+          <div className="flex flex-col gap-4 w-4/5">
+            <p className="tracking-tight text-gray-500">
+              Come & join us at Sync, for a more enhanced social experience.
+            </p>
+            <p className="tracking-tight text-gray-500">
+              At Sync we understand the need for a platform where hosters are
+              able to create and post events in real time, & for socialites to
+              explore events in real time, thereby social syncing.
+            </p>
+            <p className="tracking-tight text-gray-500">
+              The events management world is ever evolving. Our mission? To keep
+              you ahead of the game with the freshest trends, tips, and insider
+              secrets for planning exceptional events.
+            </p>
+            <p className="tracking-tight text-gray-500">
+              Join our newsletter today and get ready to dive into the community
+              of creating events in real time. Let’s craft unforgettable events
+              & create memories that last a lifetime. Stay tuned and be part of
+              future world of events management!
+            </p>
+          </div>
+          <form className="flex flex-row gap-2 w-3/5 py-3 mt-6">
+            <input
+              placeholder="Enter your email"
+              className="flex-grow border bg-white rounded px-4 text-sm"
             />
-          </a>
+            <motion.button
+              whileHover={{
+                y: -2,
+                transition: { duration: 0.1, ease: "easeOut", bounce: 0.4 },
+              }}
+              className="w-max cursor-pointer bg-blue-500 rounded py-2 flex flex-row text-white items-center gap-2 px-4 text-sm"
+            >
+              Submit <FaArrowRightLong />
+            </motion.button>
+          </form>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.6, duration: 0.7, ease: "easeInOut" }}
+          className="relative flex justify-center items-center"
+        >
+          {/* Image container */}
+          <img
+            src={phone.src}
+            className="w-[85%] rounded-xl border border-white"
+          />
+
+          {/* Elements to be placed on top of the image */}
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 1.2, duration: 0.7, ease: "easeInOut" }}
+            className="absolute top-0 -left-40 w-full h-full flex justify-center items-center"
+          >
+            <div className="text-white rounded-lg px-4 py-2 shadow-xl bg-white text-center">
+              <p className="text-base tracking-tighter text-gray-700">
+                Pool Party @ Clapham Common{" "}
+                <span className="text-xl pl-1">💦</span>
+              </p>
+            </div>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 1.6, duration: 0.7, ease: "easeInOut" }}
+            className="absolute -top-40 left-40 w-full h-full flex justify-center items-center"
+          >
+            <div className="text-white rounded-lg p-4 shadow-xl bg-white text-center">
+              <p className="text-4xl">🎉</p>
+            </div>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 2.0, duration: 0.7, ease: "easeInOut" }}
+            className="absolute top-60 left-40 w-full h-full flex justify-center items-center"
+          >
+            <div className="text-white rounded-lg px-4 py-2 shadow-xl bg-white text-center">
+              <p className="text-base tracking-tighter text-gray-700">
+                BBQ @ Harrow Youth Club 🍗
+              </p>
+            </div>
+          </motion.div>
+        </motion.div>
+      </div>
+      <div className="w-full flex flex-row justify-between py-8 items-center">
+        <div className="flex flex-row gap-2 items-center">
+          <motion.div
+            whileHover={{
+              y: -2,
+              transition: { duration: 0.1, ease: "easeOut", bounce: 0.4 },
+            }}
+            className="bg-black p-2 rounded-full cursor-pointer"
+          >
+            <FaTwitter className="text-white" />
+          </motion.div>
+          <motion.div
+            whileHover={{
+              y: -2,
+              transition: { duration: 0.1, ease: "easeOut", bounce: 0.4 },
+            }}
+            className="bg-black p-2 rounded-full cursor-pointer"
+          >
+            <FaInstagram className="text-white" />
+          </motion.div>
+          <motion.div
+            whileHover={{
+              y: -2,
+              transition: { duration: 0.1, ease: "easeOut", bounce: 0.4 },
+            }}
+            className="bg-black p-2 rounded-full cursor-pointer"
+          >
+            <FaTiktok className="text-white" />
+          </motion.div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <p className="text-xs tracking-tight text-gray-500">
+          Copyright © 2024 SYNC. All rights reserved.
+        </p>
       </div>
     </main>
   );
