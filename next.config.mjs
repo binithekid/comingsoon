d8a4280df6ff1a6e770e6a4326d40e27c129fc7b
binithekid <binitek1@gmail.com>
2024-07-26 13:41:41 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Set output to 'export' for static site export
   output: "export",
   images: {
     unoptimized: true, // Optional: Helps with static export if using images
